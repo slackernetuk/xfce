1b38d6836793b3d975cc7580d7f798c1c08e5335
Frank Honolka <slackernetuk@gmail.com>
2020-08-29 22:07:31 +0100

    cleaned

--- gtk+3/doinst.sh
+++
@@ -1,9 +0,0 @@
-
-if [ -x gtk-query-immodules-3.0 ]; then
-  gtk-query-immodules-3.0 --update-cache >/dev/null 2>&1
-fi
-
-if [ -x /usr/bin/glib-compile-schemas ]; then
-  /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas >/dev/null 2>&1
-fi
-
